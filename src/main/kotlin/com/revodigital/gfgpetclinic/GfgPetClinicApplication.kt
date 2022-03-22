4bee073db3b39d89d5ab9ea50fd3aba3a8420858
Gabriele <gabri.cabal@gmail.com>
2022-03-22 10:38:38 +0100

    🎉 First commit

---
+++ src/main/kotlin/com/revodigital/gfgpetclinic/GfgPetClinicApplication.kt
@@ -0,0 +1,11 @@
+package com.revodigital.gfgpetclinic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GfgPetClinicApplication
+
+fun main(args: Array<String>) {
+    runApplication<GfgPetClinicApplication>(*args)
+}
---
+++ src/test/kotlin/com/revodigital/gfgpetclinic/GfgPetClinicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.revodigital.gfgpetclinic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GfgPetClinicApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
